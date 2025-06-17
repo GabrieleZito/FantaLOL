266b7adde8493460f5ed7ba720e6b9f4798219c7
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-06-17 18:20:27 +0200

    boh

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<NavBar />}>
+                        <Route index element={<Home />} />
+                        <Route path="sign-in" element={<SignIn />} />
+                        <Route path="reset-password" element={<PasswordReset />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="how-to" element={<Howto />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                    <Route path="/dashboard" element={<Sidebar />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="tournaments/:tourId" element={<TournamentDetails />} />
+                        <Route path="friends" element={<Friends />} />
+                        <Route path="inbox" element={<Inbox />} />
+                        <Route path="profile" element={<Profile />} />
+                        <Route path="leaderboards" element={<Leaderboards />} />
+                        <Route path="leaderboards/:leadId" element={<LeaderboardDetails socket={socket} />} />
+                        <Route path="leaderboards/:leadId/auction" element={<Auction socket={socket} />} />
+                        <Route path="leaderboards/:leadId/team" element={<Team />} />
+                        <Route path="LEC" element={<LEC />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { NavLink, Outlet } from "react-router-dom";
+import background from "@/assets/bg.png";
+import logo from "@/assets/FantaLoL-Logo.png";
+
+export function NavBar() {
+    return (
+        <>
+            <header>
+                <nav
+                    className="flex items-center justify-between px-10 py-3"
+                    style={{
+                        backgroundImage: `url(${background})`,
+                    }}
+                >
+                    <div>
+                        <NavLink to="/">
+                            <img src={logo} className="w-20" />
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="/how-to">
+                            <div className="font-league rounded-sm border-2 border-[#38c4d7] bg-[#21262d] px-3 py-2 text-white">How It Works</div>
+                        </NavLink>
+                    </div>
+
+                    <div className="flex flex-row gap-2">
+                        <NavLink to="/login">
+                            <div className="font-league cursor-pointer rounded-sm border-2 border-[#38c4d7] bg-[#21262d] px-3 py-2 text-white">
+                                Log In
+                            </div>
+                        </NavLink>
+                        <NavLink to="/sign-in">
+                            <div className="font-league cursor-pointer rounded-sm border-2 border-[#38c4d7] bg-[#21262d] px-3 py-2 text-white">
+                                Sign Up
+                            </div>
+                        </NavLink>
+                    </div>
+                </nav>
+            </header>
+            <div className="">
+                <Outlet />
+            </div>
+        </>
+    );
+}
